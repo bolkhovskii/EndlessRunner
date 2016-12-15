07d1c630c187813444f678bca348ec95f2ddd752
Bolkhovskij <alex.bolhovskiy@yandex.ua>
2016-12-15 13:05:44 +0200

    Добавлен персонаж и камера
    Добавлено:
    -- Сцена;
    -- Персонаж;
    -- Анимация и скрипт движения игрока;
    -- Следование камеры за игроком;
    
    -- Баг дрожания камеры во время бега;

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController controller;
+    private float speed=5.0f;
+    private float gravity = 9.81f;
+    private Vector3 moveVector;
+    private float verticalVelocity = 0.0f;
+
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	
+	}
+	
+	void Update () {
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded) {
+            verticalVelocity = -0.5f;
+        } else {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        //x right and left
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        //y up and down
+        moveVector.y = verticalVelocity;
+        //z forward and backward
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
